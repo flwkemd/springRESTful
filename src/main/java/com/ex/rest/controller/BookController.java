54e3914cf70dce591071c66d164029421139849f
flwkemd <flwkemd@naver.com>
2017-12-25 23:58:32 +0100

    implemented Controller class

--- src/main/java/com/ex/rest/controller/BookController.java
+++ src/main/java/com/ex/rest/controller/BookController.java
@@ -28,4 +28,31 @@ public class BookController {
 		return ResponseEntity.ok().body("New Book has been saved with ID:" +id);
 	}
 
+	/* Get a book by id */
+	@GetMapping("/book/{id}")
+	public ResponseEntity<Book> get(@PathVariable("id") long id){
+		Book book = bookService.get(id);
+		return ResponseEntity.ok().body(book);
+	}
+	
+	/* Get all books */
+	@GetMapping("/book")
+	public ResponseEntity<List<Book>> list(){
+		List<Book> books = bookService.list();
+		return ResponseEntity.ok().body(books);
+	}
+
+	/* Update a book by id */
+	@PutMapping("/book/{id}")
+	public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody Book book){
+		bookService.update(id, book);
+		return ResponseEntity.ok().body("Book has been updated successfully");
+	}
+	
+	/* Delete a book by id */
+	@DeleteMapping("/book/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") long id){
+		bookService.delete(id);
+		return ResponseEntity.ok().body("Book has been deleted successfully.");
+	}
 }
